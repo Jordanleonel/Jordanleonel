1abd3e47cf50daa09c2896ea9cbf74d8bd08795a
Jordan Leonel <lopezalvarezleonel1234@gmail.com>
2022-08-26 11:09:49 -0500

    Practica02 11:09

---
+++ Practica02/Saludador.java
@@ -0,0 +1,7 @@
+// Defincion de clase 
+public class Saludador{
+    //metodo para saludar
+    public void saludar(){//llave que inicia el metodo 
+        System.out.println("Hola Alumno De   POO");
+    }//llave que cierra el metodo saludar 
+}//llave que cierra la clase
---
+++ Practica02/TestSaludador.java
@@ -0,0 +1,11 @@
+public class TestSaludador {
+    //metodo principal (main)
+    public static void main(String[]args){
+        //creando una instancia del Objeto saludador
+        Saludador alumno = new Saludador();
+        //accediendo al metodo saludar del alumno
+        alumno.saludar();
+         
+    }
+    
+}
