76ec74fb4c1fbcde0d1d36ee7010e541c3ad3122
Jordan Leonel <lopezalvarezleonel1234@gmail.com>
2022-09-02 08:11:17 -0500

    Practica04 8:10
    2 de septiembre del 2022.
    Inicie el laboratorio a las 8:00 am y este es el primer commit.

---
+++ Practica04/Personaje.java
@@ -0,0 +1,15 @@
+public class Personaje {
+    private String nombre;
+    private int edad;
+    public void SetNombre(String nombre){
+        this.nombre = nombre;
+    } 
+    public String getNombre(){
+        return nombre;   
+    }
+    
+    public void saludar(){
+        System.out.println("Hola Alumno de POO "+ nombre);
+        
+    }
+}
