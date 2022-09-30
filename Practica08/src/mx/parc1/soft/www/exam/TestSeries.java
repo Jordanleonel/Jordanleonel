a0b212c36c390754bf9894bb453b19bf8122ace5
Jordan Leonel <lopezalvarezleonel1234@gmail.com>
2022-09-30 08:36:12 -0500

    8:35 Primer Parcial Laboratorio de POO

---
+++ Practica08/src/mx/parc1/soft/www/exam/TestSeries.java
@@ -0,0 +1,11 @@
+package mx.parc1.soft.www.exam;
+
+public class TestSeries{
+    public static void main(String[] args) {
+        int p = s.fiboN(3, 11);
+        Serie s = new Serie();
+        s.fibo(n);
+        System.out.println(p);
+        
+    }
+}
